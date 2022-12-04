b7618d0e03e8f2ac2ec55ae62614fed4973fab25
Amir Abdullaev <amir.abdullaev@emai.cz>
2022-12-04 21:16:23 +0100

    start learning C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            byte number = 25;
+            int count = 15;
+            float totalPrice = 20.89f;
+            char character = 'A';
+            string firstName = "Amiriddin";
+            bool isEating = false;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isEating);
+        }
+    }
+}
