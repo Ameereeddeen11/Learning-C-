75f0546680dc28d14d6f0433dc1b0ae7d6185379
Amir Abdullaev <amir.abdullaev@emai.cz>
2022-12-15 10:08:57 +0100

    max value

--- pole/pole/Program.cs
+++ pole/pole/Program.cs
@@ -36,14 +36,21 @@ namespace pole
 
             Console.Write("Zadejte kolik chcete zapsat znamek: ");
             int pocet = Convert.ToInt32(Console.ReadLine()); // int32 convertuje na int
-            int[] znamky = new int[pocet];
+            int max = 0;
+            int[] znamky = new int[pocet];      
 
             for(int i = 0; i < pocet; i++)
             {
                 Console.Write("zadejte " + (i + 1) + ". cislo");
                 znamky[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (max< znamky[i])
+                {
+                    max = znamky[i];
+                }
             }
             Console.WriteLine("Prumer znamek je: {0}", znamky.Average()); //average je prumer
+            Console.WriteLine("Nejvet3i cislo je " + max);
 
             Console.ReadLine();
         }
