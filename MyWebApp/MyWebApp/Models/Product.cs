261333f9a30ab5091256b760946812d2428a8a02
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-01-14 22:38:19 +0100

    start learning WinForms in C#

---
+++ MyWebApp/MyWebApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace MyWebApp.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
