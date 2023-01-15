dce2c70d2eeeb7c80a2e1e9ae6007e3e5282b7b2
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-01-15 21:31:53 +0100

    create mini calculator to caltulate content and girth of square, rectangle

--- LearningWinForm/Form1.cs
+++ LearningWinForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace LearningWinForm
 {
@@ -17,9 +18,43 @@ namespace LearningWinForm
             InitializeComponent();
         }
 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                //obvod ctverce
+                int a = int.Parse(textBox1.Text);
+                int c = a * 4;
+                label3.Text = "Odpoved je " + c;
+            }
+            else if (radioButton2.Checked)
+            {
+                //obvod obdelniku
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                int c = 2 * (a + b);
+                label3.Text = "Odpoved je " + c;
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Text = "Clicked";
+            if (radioButton1.Checked)
+            {
+                //obsah ctverce
+                int a = int.Parse(textBox1.Text);
+                int c = a * a;
+                label3.Text = "Odpoved je " + c;
+            }
+            else if (radioButton2.Checked)
+            {
+                //obsah obdelniku
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                int c = a * b;
+                label3.Text = "Odpoved je " + c;
+            }
         }
     }
 }
