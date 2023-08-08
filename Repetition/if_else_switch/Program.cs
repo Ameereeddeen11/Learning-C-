7c94bf3700db9e53efacb49f9d0ebe5a613bc55a
Amir Abdullaev <amir.abdullaev@emai.cz>
2023-08-08 22:05:48 +0200

    stil repetition C#

---
+++ Repetition/if_else_switch/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace if_else_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            string result = (age >= 18) ? "You can vote" : "You con't vote";
+            Console.WriteLine(result);
+
+            Console.WriteLine("What is time now? ");
+            int time = Convert.ToInt32(Console.ReadLine());
+            if (10 >= time)
+            {
+                Console.WriteLine("Good morning");
+            }
+            else if (14 >= time)
+            {
+                Console.WriteLine("Good day");
+            }
+            else if (15 <= time)
+            {
+                Console.WriteLine("Good aftermoon");
+            }
+            Console.ReadLine();
+        }
+    }
+}
