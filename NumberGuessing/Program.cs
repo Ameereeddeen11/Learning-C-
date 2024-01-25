80cecc2f6695518616144a466de129bcc0daca59
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-01-25 22:00:08 +0100

    a little bit edit the code and add some function

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -5,35 +5,54 @@ namespace NumberGuessing
     {
         static void Main(string[] args)
         {
-            bool isResultCorrect = false;
-            Random random = new Random();
-            int randomnum = random.Next(1, 10);
-            Console.WriteLine("Hello! Lets play Guessing number game.");
-            Console.WriteLine("I think a random number between 1 and 10. You have to guess the number.");
-            Console.WriteLine("Please enter any key to start game");
-            Console.ReadKey();
-            Console.WriteLine("Hmm... Let me to thing the number");
-            System.Threading.Thread.Sleep(2000);
-            while (!isResultCorrect)
+            bool run = false;
+            while (!run)
             {
-                Console.WriteLine("I thought a number. Please enter your guess");
-                int guess = Convert.ToInt32(Console.ReadLine());
-                if (guess > randomnum)
+                bool isResultCorrect = false;
+                Random random = new Random();
+                int randomnum = random.Next(1, 10);
+                Console.WriteLine("Hello! Lets play Guessing number game.");
+                Console.WriteLine("I think a random number between 1 and 10. You have to guess the number.");
+                Console.WriteLine("Please enter any key to start game");
+                Console.ReadKey();
+                Console.WriteLine("Hmm... Let me to thing the number");
+                System.Threading.Thread.Sleep(2000);
+                while (!isResultCorrect)
                 {
-                    Console.WriteLine("Your guess is high");
+                    Console.WriteLine("I thought a number. Please enter your guess");
+                    int guess = Convert.ToInt32(Console.ReadLine());
+                    if (guess > randomnum)
+                    {
+                        Console.WriteLine("Your guess is high");
+                    }
+                    else if (guess < randomnum)
+                    {
+                        Console.WriteLine("Your guess is low");
+                    }
+                    else if (guess == randomnum)
+                    {
+                        Console.WriteLine("Your guess is correct");
+                        isResultCorrect = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid number");
+                    }
                 }
-                else if (guess < randomnum)
+
+                Console.WriteLine("Congratulations! You won the game.");
+                Console.WriteLine("Do you want to play again?");
+                Console.WriteLine("Press 1 to play again. Press any key to exit");
+                string playAgain = Console.ReadLine();
+                if (playAgain == "1")
                 {
-                    Console.WriteLine("Your guess is low");
+                    run = false;
                 }
                 else
                 {
-                    Console.WriteLine("Correct! I guessed " + randomnum);
-                    isResultCorrect = true;
+                    run = true;
                 }
             }
-
-            Console.WriteLine("Congratulations! You won the game.");
             Console.ReadKey();
         }
     }
